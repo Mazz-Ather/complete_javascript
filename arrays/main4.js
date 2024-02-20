39f439003e910f8b2eac2552cc8e439ee41ac4ae
Mazz-Ather <mazzather@gmail.com>
2024-02-20 13:36:28 -0800

    complete the topic of array

--- arrays/main4.js
+++ arrays/main4.js
@@ -23,3 +23,30 @@ for (let i = 0 ; i <items.length; i++){
     items[i] -= offer;
 }
 console.log(items);
+
+// concat
+let heroes = ["imran khan","muhammad tahir","andrew tate"];
+let friend = ["saqib","junaid"];
+let myName = ["mazz"];
+console.log(heroes.concat(friend,myName));
+
+//method, add (push,unshift) &&delete( pop & shift,)
+let deleted = friend.shift()
+console.log("deleted item ",deleted);
+console.log(heroes.concat(friend,myName));
+
+let SLICE = heroes.slice(0,2)
+// console.log(SLICE);
+
+let h = [1,2,3,4,5,6,7];
+h.splice(2,0,102)
+console.log(h);
+
+//?practice
+let companies = ["google","microsoft","ibm","facebook","shopify"];
+companies.shift()
+console.log(companies);
+companies.splice(1,1,"ola");
+console.log(companies);
+companies.push("amazon")
+console.log(companies);
